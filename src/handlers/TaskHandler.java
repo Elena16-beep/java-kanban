45309634973dc0f04fd309b34c1318ace1090190
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2025-07-11 18:26:31 +0500

    Доработан доступ к методам программы через HTTP API. CheckStyle v.1 (спринт 9)

--- src/handlers/TaskHandler.java
+++ src/handlers/TaskHandler.java
@@ -1,8 +1,6 @@
 package handlers;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import service.TaskManager;
 import model.Task;
 import com.sun.net.httpserver.HttpExchange;
